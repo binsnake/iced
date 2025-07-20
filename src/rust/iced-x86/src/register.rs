3a423d3f5b8e29a95625801c05c7430b11fc1e4c
bombaris34 <reapsgg@proton.me>
2025-07-20 18:26:51 +0200

    Update register.rs

--- src/rust/iced-x86/src/register.rs
+++ src/rust/iced-x86/src/register.rs
@@ -937,6 +937,7 @@ impl Register {
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[cfg_attr(not(feature = "exhaustive_enums"), non_exhaustive)]
 #[allow(missing_docs)]
+#[repr(C)]
 pub enum Register {
 	None = 0,
 	AL = 1,
